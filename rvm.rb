be304c83f58ab7dc096c4de2e255e957e389de98
Pavel Gabriel <alovak@gmail.com>
2011-08-03 11:55:14 +0300

    added rvm deps

---
+++ rvm.rb
@@ -0,0 +1,35 @@
+dep 'rvm installed' do
+  requires 'curl.managed', 'rvm requirements'
+  met? { File.exist?("/usr/local/rvm") }
+  meet do
+    sudo "bash < <(curl -sk https://rvm.beginrescueend.com/install/rvm)"
+  end
+end
+
+dep 'rvm requirements' do
+  requires %w(build-essential.managed bison.managed openssl.managed libreadline6.managed libreadline6-dev.managed curl.managed zlib1g.managed libssl-dev.managed libyaml-dev.managed sqlite3.managed libxml2-dev.managed libxslt-dev.managed)
+end
+
+dep 'rvm set group for user' do
+  before { var(:rvm_username, :default => shell("whoami")) }
+  requires 'rvm group exists'
+  met? { shell("groups #{var(:rvm_username)}").split(" ").include?("rvm") }
+  meet { sudo("adduser #{var(:rvm_username)} rvm") }
+end
+
+dep 'rvm ruby installed' do
+  before { var(:default_ruby, :default => "1.9.2") }
+  met? { shell("rvm list").include?(var(:default_ruby)) }
+  meet {
+    log_shell "Installing #{var(:default_ruby)} with rvm", "rvm install #{var(:default_ruby)}"
+  }
+end
+
+dep 'rvm setup default ruby ' do
+  before { var(:default_ruby, :default => "1.9.2") }
+  requires 'rvm ruby installed'
+  met? { shell("rvm list").include?("=>") }
+  meet {
+    log_shell "Installing #{var(:default_ruby)} with rvm", "rvm use #{var(:default_ruby)} --default"
+  }
+end
